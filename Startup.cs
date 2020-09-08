06a78a76ebdc56922a82d53ad1deb8fea0938280
serbingeorg <serbingeorg@gmail.com>
2020-09-08 22:43:35 +0300

    solved the problem with the swagger

--- Startup.cs
+++ Startup.cs
@@ -34,27 +34,7 @@ namespace QuestionnareTestTask
             opt.UseInMemoryDatabase("Questionnaire_DB"));
             services.AddControllers();
             services.AddSwaggerGen();
-                //c =>
-                //{
-                //    c.SwaggerDoc("v1", new OpenApiInfo
-                //    {
-                //        Version = "v1",
-                //        Title = "ToDo API",
-                //        Description = "A simple example ASP.NET Core Web API",
-                //        TermsOfService = new Uri("https://example.com/terms"),
-                //        Contact = new OpenApiContact
-                //        {
-                //            Name = "Shayne Boyer",
-                //            Email = string.Empty,
-                //            Url = new Uri("https://twitter.com/spboyer"),
-                //        },
-                //        License = new OpenApiLicense
-                //        {
-                //            Name = "Use under LICX",
-                //            Url = new Uri("https://example.com/license"),
-                //        }
-                //    });
-                //});
+               
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,7 +44,7 @@ namespace QuestionnareTestTask
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Questionnaire");
-               // c.RoutePrefix = string.Empty;
+                c.RoutePrefix = string.Empty;
             });
 
             if (env.IsDevelopment())
