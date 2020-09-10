b69acd13a69a0c0e5e17a91a406c34b734267a27
serbingeorg <serbingeorg@gmail.com>
2020-09-10 21:06:26 +0300

    little changes

---
+++ Models/DTO/QuestionResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionnareTestTask.Models.DTO
+{
+    public class QuestionResponse
+    {
+        public string Body { get; set; }
+    }
+}
