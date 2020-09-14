64baaa670b2b51512942e460e3c6145dc31621ca
serbingeorg <serbingeorg@gmail.com>
2020-09-14 22:22:32 +0300

    + request

---
+++ ApiContracts/Request/AnswerRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionnareTestTask.ApiContracts.Request
+{
+    public class AnswerRequest
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public int TimesSelected { get; set; }
+    }
+}
